a2564a8a1a74cf48eb0412052bcfe33dbebe2496
AbubakarCinnova <mahboobabubakar786@gmail.com>
2023-09-30 16:33:59 +0500

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -9,7 +9,7 @@ const ProductList = () => {
   const dispatch = useDispatch();
   const fetchProduct = async () => {
     const response = await axios
-      .get("https://fakestoreapi.com/products")
+      .get("https://fakestoreapi.com/products?limit=8")
       .catch((err) => {
         console.log(err);
       });
@@ -25,7 +25,7 @@ const ProductList = () => {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 text-left ">
-            Products
+            Featured Products
           </h2>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
